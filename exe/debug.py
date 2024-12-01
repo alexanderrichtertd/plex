90363d7c16056c0be31509b80fb7da337b8a165d
Alexander Richter <alexanderrichtertd@gmail.com>
2024-12-01 10:58:15 +0100

    defined debug

--- exe/debug.py
+++ exe/debug.py
@@ -1,6 +1,6 @@
 #*********************************************************************
 # content   = Testing environment
-# date      = 2024-11-22
+# date      = 2024-12-01
 #
 # license   = MIT <https://github.com/alexanderrichtertd>
 # author    = Alexander Richter <alexanderrichtertd.com>
@@ -9,32 +9,9 @@
 import os
 import sys
 
-pipeline_path = f'{os.path.dirname(os.path.dirname(__file__))}/scripts'
-sys.path.append(pipeline_path)
+plex_path = f'{os.path.dirname(os.path.dirname(__file__))}/scripts'
+sys.path.append(plex_path)
 
 # SETUP pipeline
-import pipeline
-pipeline.Setup()
-
-# MOST important module
 from plex import Plex
-# Plex().software.name = 'maya'
-# print(Plex().config_software)
-
-
-#*********************************************************************
-# TESTING
-
-# START software
-software_name = 'maya'
-# from software import Software
-# Plex().software.start(software_name)
-# import arConfig
-# arConfig.start()
-
-
-open_path = r'G:\My Drive\3 RESOURCES\github\plex\project\4_assets\assetName\GEO\WORK/assetName_GEO_v002.ma'
-# Plex().start_software(dcc_name, open_path)
-
-
-print(Plex().software)
+Plex().setup()
